84efcb1b09b282b3e253c6736759147c4cc72711
slobodan-mirkovic <mirkovic.sloba@gmail.com>
2015-10-28 21:02:21 +0100

    lightbox

--- js/app.js
+++ js/app.js
@@ -2,7 +2,24 @@
 
 $(document).ready(function () {
 	
+	var $overlay = $('<div id="overlay"></div>');
+	var $img = $('<img width="700px" height="465px">');	
+
+	$overlay.append($img);
+
+	$("body").append($overlay)
+
 	$("#inventory a").click(function (event) {
 		event.preventDefault();
+		var href = $(this).attr("href");
+		$img.attr("src", href);
+
+		$overlay.show();
+
 	});
+
+	
+	$("#overlay").click(function () {
+	$overlay.hide();
+});
 });
